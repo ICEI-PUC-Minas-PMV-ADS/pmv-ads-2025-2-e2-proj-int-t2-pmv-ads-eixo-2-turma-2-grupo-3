b68c1261738393f555d797e926c594c7819cf113
brenoeller <brenoeller@hotmail.com>
2025-11-07 15:52:20 -0300

    feat: cria home e redireciona o login para ela

---
+++ src/Cuida+/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cuida_.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("home")]
+        public IActionResult Index()
+        {
+            return View("Home");
+        }
+    }
+}
