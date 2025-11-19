088508d4c57ad59dd88117d7b3bc54c4fa382c0c
CaioRezCosta <caiorcosta12@gmail.com>
2025-11-19 18:35:05 -0300

    Adding in Cadastro, adding model, adjustments

---
+++ src/Cuida+/Models/Especialidades.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cuida_.Models
+{
+    [Table("especialidades")]
+    public class Especialidades
+    {
+        [Key]
+        [Column("id")]
+        public int id { get; set; }
+
+        [Column("crm")]
+        public string crm { get; set; }
+    }
+}
